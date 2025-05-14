71588e5b6d4005f9edd8e1a9ee2b85c586df9154
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2025-05-14 14:53:53 -0300

    Adiciona projeto

---
+++ src/test/java/br/com/gustavo/CursoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
